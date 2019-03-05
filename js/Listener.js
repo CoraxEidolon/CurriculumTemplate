1f524e9bfa3064fb6d07f14688799c7bd4a9e314
CoraxEidolon <37768035+CoraxEidolon@users.noreply.github.com>
2019-03-06 01:53:40 +0300

    Первый коммит

---
+++ js/Listener.js
@@ -0,0 +1,18 @@
+function onLoad() {
+    /*document.getElementById("Matrix").addEventListener("click", OnOffLed);
+    document.getElementById("MatrixSize").addEventListener("change", MatrixSize); */
+    var DropZone=document.getElementById("DropZone");
+    DropZone.addEventListener("drop", dropHandler);
+    DropZone.addEventListener("dragover", dragOverHandler);
+    DropZone.addEventListener("dragleave", onMouseOutDropZone);
+    DropZone.addEventListener("drop",onMouseOutDropZone);
+    DropZone.addEventListener("click",selectFile);
+
+
+
+    document.getElementById("Input_SelectFile").addEventListener("change", getSelectFile);
+
+
+
+
+}
